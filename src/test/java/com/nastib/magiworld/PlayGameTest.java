11a228cf09576ca274069dd92023ee2cc943205a
ADMIN <nastib@outlook.com>
2018-09-12 15:47:22 +0100

    Commit magiworld new tests Signed-off-by:ADMIN <nastib@outlook.com>

---
+++ src/test/java/com/nastib/magiworld/PlayGameTest.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.nastib.magiworld;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class PlayGameTest {
+    
+    public PlayGameTest() {
+    }
+
+    /**
+     * Test of play method, of class PlayGame.
+     */
+    @Test
+    public void testPlay() {
+        System.out.println("play");
+        PlayGame instance = null;
+        instance.play();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
---
+++ src/test/java/com/nastib/magiworld/TheEndTest.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.nastib.magiworld;
+
+import com.nastib.magiworld.bean.Person;
+import java.util.List;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class TheEndTest {
+    
+    public TheEndTest() {
+    }
+
+    /**
+     * Test of end method, of class TheEnd.
+     */
+    @Test
+    public void testEnd() {
+        System.out.println("end");
+        List<Person> persons = null;
+        TheEnd.end(persons);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
